c2492727219f722b75d6aef1ed4a236d00bed323
Thanapat Veerakajorn <t.veerakajorn@gmail.com>
2020-12-18 22:36:22 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Add your functions here
+function map(sourceArray, sourcefunc){
+    let x = []
+    for (let y of sourceArray){
+        x.push(sourcefunc(y))
+    }
+    return x
+};
+
+function reduce(sourceArray, sourcefunc, startingvalue){
+    let x = !!startingvalue ? startingvalue : sourceArray[0]
+    let y = !!startingvalue ? 0 : 1
+
+    for (; y < sourceArray.length; y++){
+        x = sourcefunc(sourceArray[y], x)
+    }
+    return x
+}; 
